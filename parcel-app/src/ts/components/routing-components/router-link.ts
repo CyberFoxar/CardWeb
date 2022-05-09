3c348ac2fed715df877d0972462bb61150e144c9
CyberFoxar <cyberfroxar@gmail.com>
2022-05-09 17:22:11 +0200

    lit router

---
+++ parcel-app/src/ts/components/routing-components/router-link.ts
@@ -0,0 +1,39 @@
+import { LitElement, html } from 'lit';
+
+import { customElement, property } from 'lit/decorators.js';
+import { outlet, router, navigator, Routes } from 'lit-element-router';
+
+
+@customElement('router-link')
+class RouterLinkComponent extends navigator(LitElement) {
+
+    @property()
+    public href: string;
+
+    constructor() {
+        super();
+        this.href = "";
+    }
+
+    render() {
+
+        return html`
+    <a href="${this.href}" @click="${this.linkClick}">
+        <slot></slot>
+    </a>
+  `;
+
+    }
+
+    linkClick(event: any) {
+        event.preventDefault();
+        this.navigate(this.href);
+    }
+
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+      "router-link": RouterLinkComponent;
+    }
+}
